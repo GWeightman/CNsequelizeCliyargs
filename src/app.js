e7e31039df9cc3176ae8e7da274b4407a6a7715d
GWeightman <g.weightman@hotmail.co.uk>
2022-01-05 16:19:01 +0000

    minor updates

--- src/app.js
+++ src/app.js
@@ -4,40 +4,34 @@ const { addBook, deleteBook, editBook, editAuthor, wholebookTable } = require(".
 
 const app = async (args) => {
     try {
-        if (args.addmovie) { /*--addmovie --title "title" --actor "actor"*/
+        if (args.addentry) { /*--addentry --title "title" --actor "actor" --author "author"*/
             const movieObj = {title: args.title, actor: args.actor}
+            const bookObj = {title: args.title, author: args.author}
+            await addBook(bookObj)
             await addMovie(movieObj)
         }
-        else if (args.deletemovie) { /*--deletemovie --title "title"*/
+        else if (args.deleteentry) { /*--deleteentry --title "title"*/
             const movieObj = {title: args.title}
+            const bookObj = {title: args.title}
             await deleteMovie(movieObj)
+            await deleteBook(bookObj)
         }
         else if (args.edittitle) { /*--edittitle --title "title" --new "newtitle"*/
             const movieObj = {title: args.title, new: args.new}
+            const bookObj = {title: args.title, new: args.new}
+            await editBook(bookObj)
             await editTitle(movieObj)
         }
         else if (args.editactor) { /*--editactor --actor "actor" --new "newactor"*/
             const movieObj = {actor: args.actor, new: args.new}
             await editActor(movieObj)
         }
-        else if (args.movielist) {
-            await wholeTable()
-        }
-        else if (args.addbook) { /*--addbook --title "title" --author "author"*/
-            const bookObj = {title: args.title, author: args.author}
-            await addBook(bookObj)
-        }
-        else if (args.deletebook) { /*--deletebook --title "title"*/
-            const bookObj = {title: args.title}
-            await deleteBook(bookObj)
-        }
-        else if (args.editbook) { /*--editbook --title "title" --new "newtitle"*/
-            const bookObj = {title: args.title, new: args.new}
-            await editBook(bookObj)
-        }
         else if (args.editauthor) { /*--editauthor --author "author" --new "newauthor"*/
             const bookObj = {author: args.author, new: args.new}
             await editAuthor(bookObj)
+        }
+        else if (args.movielist) {
+            await wholeTable()
         }
         else if (args.booklist) {
             await wholebookTable()
